0f8dfc19adca1e3976058b4d112b0151bc146392
Jack Duignan <jackpduignan@gmail.com>
2026-09-06 15:51:06 +1200

    Removed ui

--- ui/src-tauri/src/commands/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod sentinel2;
-pub mod sentinel1;
-pub mod weather;
-pub mod ais;
--- ui/src/tabs/sentinel1.js
+++
@@ -1,9 +0,0 @@
-export function initSentinel1Tab() {
-  document.getElementById('panel-sentinel1').innerHTML = `
-    <div class="stub">
-      <div class="stub-title">Sentinel-1 SAR</div>
-      <div class="stub-badge">COMING SOON</div>
-      <p>Wire up the Sentinel-1 GRD collection in <code>src-tauri/src/commands/sentinel1.rs</code></p>
-    </div>
-  `;
-}
